9a487dcc1cc1be8c1c8c52699a6d0f88e072f89e
LYDHM <liuyoudehama@gmail.com>
2020-04-02 16:44:08 -0700

    Chapter3: Use Interface Blocks.

--- shaders/playground.vert.glsl
+++ shaders/playground.vert.glsl
@@ -3,7 +3,10 @@
 layout (location = 0) in vec4 offset;
 layout (location = 1) in vec4 color;
 
-out vec4 vs_color;
+out VS_OUT
+{
+	vec4 color;
+} vs_out;
 
 void main(void)
 {
@@ -12,5 +15,5 @@ void main(void)
     								vec4(0.25, 0.25, 0.5, 1.0));
 
 	gl_Position = vertices[gl_VertexID] + offset;
-	vs_color = color;
+	vs_out.color = color;
 }
